d8efffd86cc470207f63e5c3d881b3935050abc4
c1337 <cyril199897@gmail.com>
2022-05-05 03:08:35 +0530

    User Model nc

--- MessageHandle/messageHandler.js
+++ MessageHandle/messageHandler.js
@@ -1,30 +1,85 @@
-const messageModel = require("../Model/Model_message");
-
+const model_user = require("../Model/Model_user");
+const  {client} = require("../index")
 
 const messageHandler = function(message){
 
     
     console.log(message.body)
-
+    console.log(message.from)
+    client.sendMessage(message.from,"baam from client ");
     const myArray = message.body.split("@#");
     var tag = myArray[1];
     var data = myArray[0];
-    var message = messageModel({
-        data : data,
-        tag : tag,
-        time : "String",
-    });
-    messageModel.find({ "tag": tag}).count(function (err, res) {
+    // var messageDb = messageModel({
+    //     username:message.from,
+    //     data : data,
+    //     tag : tag,
+    //     time : "String",
+    // });
+    messageModel.find({ "username": message.from}).count(function (err, res) {
         if (err)
            throw err;
            console.log("count of ",tag," ",res)
+        if(count>0){
+            messageModel.find({
+                "username": message.from,
+                tags:{
+                    $elemMatch:{
+                        threadName:tag
+                    }
+                }
+            }, function(err,res){
+                // check if the tag exists
+                // insert tag if not exists
+                
+                // update tag array if exists 
+            })
+
+
+            // below code inserts doc if not exists
+            var newTagelement = {
+                threadName:tag,
+                threads:[{
+                    message:data,
+                    time:"time now"
+                }]
+            }; 
+           
+            model_user.updateOne(
+                { username: message.from }, 
+                { $push: { tags: newTagelement } },
+                done
+            );
+            
+        }
+        else{
+            // user message for first time
+            var user_in_db = model_user({
+                username : message.from,
+    tags: [
+        {    
+            threadName : tag,
+            threads:
+            [
+                {
+                    message:data,
+                    time:"String"
+                }
+            ]
+            
+        }
+    ]
+            });
+            user_in_db.save()
+            .then(()=>console.log("Account data is passed to MessageSchema"))
+            .catch((error)=>console.log(error))
+            
+        }
         // db.close();
     });
 
     
-    message.save()
-    .then(()=>console.log("Account data is passed to MessageSchema"))
-    .catch((error)=>console.log(error))
+    
     
     
     
---
+++ Model/Model_user.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+
+
+
+// console.log(process.env.mongoDbUrl)
+const url = process.env.mongoDbUrl;
+mongoose.connect(process.env.mongoDbUrl, {useNewUrlParser:true, useUnifiedTopology:true})
+.then(()=>console.log("Message Schema is ONLINE"))
+.catch((error)=>console.log("Error in message.js"+error))
+
+
+
+
+const Schema = mongoose.Schema;
+
+const userDb = new Schema({
+    username : 
+    { 
+        
+        type:String, lowercase:true, unique:true
+    },
+    tags:
+    [
+        {    
+            threadName : String,
+            threads:
+            [
+                {
+                    message:String,
+                    time:String
+                }
+            ]
+            
+        }
+    ]     
+})
+
+var DatainDb = mongoose.model('Data',datainDb);
+
+
+module.exports = DatainDb;
+
+const db = mongoose.connection;
+db.once('error',(err)=>{
+    console.log("Error at Accountdata"+err);
+});
+db.on('open',()=>{
+    console.log("Accountdata DB is connected");
+})
